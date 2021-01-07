a5d8c2b24aa97d74c3dee72957ba51573f26f77e
Marc Loegel <loegel@swms.de>
2021-01-07 12:42:35 +0100

    Added comments to IChatClient

--- SignalPaint/Hubs/IChatClient.cs
+++ SignalPaint/Hubs/IChatClient.cs
@@ -4,12 +4,48 @@ namespace SignalPaint.Hubs
 {
   public interface IChatClient
   {
+    /// <summary>
+    /// Used to send a chat-message
+    /// </summary>
     Task ReceiveMessage(string user, string message);
-    Task SignalTouchStart(string id, float x, float y, float lineWidth, string color);
-    Task SignalTouchMove(string id, float x, float y, float lineWidth);
-    Task SignalTouchEnd(string id, float x, float y);
-    //Task SignalSetColor(string id, string newColor);
-    Task SignalClearScreen(string id);
-    Task SignalDisconnected(string id);
+
+    /// <summary>
+    /// Starts drawing
+    /// </summary>
+    /// <param name="connectionId">ID of the connection</param>
+    /// <param name="x">Screen coordinate x</param>
+    /// <param name="y">Screen coordinate x</param>
+    /// <param name="lineWidth">Line width based on pen/finger pressure if available</param>
+    /// <param name="color">Line color</param>
+    Task SignalTouchStart(string connectionId, float x, float y, float lineWidth, string color);
+
+    /// <summary>
+    /// Contiue drawing
+    /// </summary>
+    /// <param name="connectionId">ID of the connection</param>
+    /// <param name="x">Screen coordinate x</param>
+    /// <param name="y">Screen coordinate x</param>
+    /// <param name="lineWidth">Line width based on pen/finger pressure if available</param>
+    Task SignalTouchMove(string connectionId, float x, float y, float lineWidth);
+
+    /// <summary>
+    /// Finish drawing
+    /// </summary>
+    /// <param name="connectionId">ID of the connection</param>
+    /// <param name="x">Screen coordinate x</param>
+    /// <param name="y">Screen coordinate x</param>
+    Task SignalTouchEnd(string connectionId, float x, float y);
+
+    /// <summary>
+    /// Clear the canvas
+    /// </summary>
+    /// <param name="connectionId">ID of the connection</param>
+    Task SignalClearScreen(string connectionId);
+
+    /// <summary>
+    /// Notify that a client hast disconnected
+    /// </summary>
+    /// <param name="connectionId">ID of the connection</param>
+    Task SignalDisconnected(string connectionId);
   }
 }
